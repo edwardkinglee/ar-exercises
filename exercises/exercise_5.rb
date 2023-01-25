678d9e1fee824d1080096955a06a1861ddced8d9
edwardkinglee <edward.lee3@telus.com>
2023-01-25 16:10:58 -0500

    Complete exercise6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "The total revenue for all stores is $#{@total_revenue}"
+
+@average_revenue = Store.average(:annual_revenue)
+puts "The average revenue for all stores is $#{@average_revenue}"
+
+@count_store_1M_revenue = Store.where("annual_revenue > 1000000").count
+puts "The number of stores that have over 1 million in revenue are #{@count_store_1M_revenue}."
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 40)
+@store1.employees.create(first_name: "Edward", last_name: "Lee", hourly_rate: 150)
+
+@store2.employees.create(first_name: "Joey", last_name: "Wong", hourly_rate: 40)
+@store2.employees.create(first_name: "Sunny", last_name: "Jones", hourly_rate: 60)
+@store2.employees.create(first_name: "Dina", last_name: "Bullari", hourly_rate: 70)
